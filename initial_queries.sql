a5a51bc7338daa13b91f710e7d73e7847c441820
Khusniddin <khusniddinsobirjonov4@gmail.com>
2025-10-26 11:25:37 +0500

    Initial commit

---
+++ initial_queries.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE car_prices
+
+CREATE TABLE car_prices (
+    year INT,
+    brand TEXT,
+    model TEXT,
+    trim TEXT,
+    body text,
+    transission VARCHAR(15),
+    vin TEXT,
+    state VARCHAR(3),
+    condition INT,
+    odometer INT,
+    color TEXT,
+    interior TEXT, 
+    seller TEXT,
+    mmr INT, 
+    sellingprice NUMERIC,
+    saledate DATE
+)
+
+COPY car_prices 
+FROM '/Users/macbook/Documents/Data_Analytics/Car_prices/car_prices.csv' 
+DELIMITER ',' CSV HEADER;
+
+ALTER TABLE car_prices
+ALTER COLUMN state TYPE TEXT
+
+DROP TABLE car_prices
+
+\copy car_prices FROM '/Users/macbook/Documents/Data_Analytics/Car_prices/car_prices.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+SELECT *
+FROM car_prices
+limit 100
